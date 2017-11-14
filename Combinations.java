fd457ad7af9f329738a7cbb978b6b7502e84971a
neX- <hoanglongtran0698@gmail.com>
2017-11-14 10:39:45 +0700

    Initial commit

---
+++ Combinations.java
@@ -0,0 +1,14 @@
+package Assignments;
+
+public class Combinations {
+    public static void main(String[] args) {
+        int[] arr;
+        System.out.printf("Please enter the array:");
+
+        System.out.printf("Please enter the sum:");
+    }
+    public static void printCombinations(int[] arr, int sum) {
+
+    }
+
+}
