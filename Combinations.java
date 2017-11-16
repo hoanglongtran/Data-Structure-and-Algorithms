3169d9cafeb99b83ddb125b76ad37560af42554f
neX- <hoanglongtran0698@gmail.com>
2017-11-16 15:54:25 +0700

    blah blah

--- Combinations.java
+++ Combinations.java
@@ -1,13 +1,34 @@
 package Assignments;
-
+import java.util.*;
 public class Combinations {
     public static void main(String[] args) {
-        int[] arr;
-        System.out.printf("Please enter the array:");
 
-        System.out.printf("Please enter the sum:");
+        System.out.printf("Please enter the array:");
+        int[] a = {1};
+        ArrayList<Integer> soFar = new ArrayList<>();
+        printCombinations(soFar,a);
+        //getInput();
     }
-    public static void printCombinations(int[] arr, int sum) {
+    /*public static List<Integer> getInput() {
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> input = new ArrayList<>();
+        while   (in.hasNextInt()){
+            int i = in.nextInt();
+            input.add(i);
+        }
+
+        return input;
+    }*/
+    public static void printCombinations(ArrayList<Integer> current, int[]arr ) {
+        if (arr.equals(0)){
+            System.out.println(current);
+        }
+        else {
+
+            printCombinations(new ArrayList<Integer>(current) {{ add(arr[0]); }}, Arrays.copyOfRange(arr, 1, arr.length) );
+
+            printCombinations(current, Arrays.copyOfRange(arr, 1, arr.length));
+        }
 
     }
 
