4a3a9126e747935de684c2bcec7109a877eeb8de
s3635165 <hoanglongtran0698@gmail.com>
2017-11-17 06:34:43 +0700

    nearly nearly finish

--- Combinations.java
+++ Combinations.java
@@ -1,7 +1,21 @@
+/*
+  RMIT University Vietnam
+  Course: COSC2658 Data Structures & Algorithms
+  Semester: 2017C
+  Assignment: 1
+  Author: Long Hoang Tran
+  ID: s3635165
+  Created date: 17/11/2017
+
+ Acknowledgement: https://stackoverflow.com/questions/4632322/finding-all-possible-combinations-of-numbers-to-reach-a-given-sum
+                  https://www.youtube.com/watch?v=NdF1QDTRkck
+
+*/
+
 import java.util.*;
 public class Combinations {
     //public static int currentSum = 0;
-    public static ArrayList<Integer> usedNumb = new ArrayList<>();
+    private static ArrayList<Integer> usedNumb = new ArrayList<>();
     public static void main( String[] args ){
 
         int arr[] = {1, 3, 2,2};
@@ -16,76 +30,21 @@ public class Combinations {
     }
 
     public static void printCombinations(int[] arr, int sum){
-
         for (int i = 0; i < arr.length; i++){
-            //System.out.println("Goal " + goal);
-            //System.out.println("Current item: " + availableItems[i]);
             int[] newReducedArray = Arrays.copyOfRange(arr, 1 + i , arr.length);
-
             if (sum - arr[i] == 0){
-
-                //System.out.println("Current item " + availableItems[i]);
-                //usedNumb.add(arr[i]);
                 usedNumb.add(arr[i]);
                 System.out.println(Arrays.toString(usedNumb.toArray()));
                 usedNumb.remove(usedNumb.size() - 1);
-                //currentSum = 0;
-                //break;
             }
-
             if(sum < arr[i] ){
                 continue;
             }
             usedNumb.add(arr[i]);
             if(sum - arr[i] > 0){
-                //currentSum += availableItems[i];
-
-                //System.out.println("Current goal " + (goal - availableItems[i]));
-
-
-
                 printCombinations(newReducedArray, sum - arr[i]);
             }
             usedNumb.remove(usedNumb.size() -1 );
-
         }
     }
-
-    /*public static void printCombinations(int arr[], int sum){
-        int count = 0;
-        int actualSum = sum;
-        while (count < arr.length) {
-            int j = 0;
-            int arrCollection[] = new int[arr.length];
-            for (int k = 0; k < arrCollection.length; k++){
-                arrCollection[k] = -99; // as the array can contain only +ve integers
-            }
-            for (int i = count; i < arr.length; i++) {
-                sum = sum - arr[i];
-                if (sum < 0){
-                    sum = sum + arr[i];
-                } else if (sum > 0){
-                    arrCollection[j++] = arr[i];
-                } else if (sum == 0){
-                    System.out.println("");
-                    arrCollection[j++] = arr[i];
-                    int countElements = 0;
-                    for (int k = 0; k < arrCollection.length; k++){
-                        if (arrCollection[k] != -99) {
-                            countElements++;
-                            System.out.print(arrCollection[k] + " ");
-                        }
-                    }
-                    if (countElements == 1){
-                        i = arr.length -1;
-                    }
-                    sum = sum + arr[i];
-                    j--;
-                }
-            }
-            count++;
-            sum = actualSum;
-        }
-    }*/
-
 }
